3b85189d2bc1b97e0824c51a44051cc417065480
Anushree <anushreebhatt357@gmail.com>
2025-03-17 18:10:27 -0400

    Initial setup

---
+++ src/main/java/com/practice/springboot/StudyGrind/StudyGrindApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.StudyGrind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGrindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyGrindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/springboot/StudyGrind/StudyGrindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.StudyGrind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGrindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
