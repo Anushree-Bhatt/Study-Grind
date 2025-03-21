3dfb7050f404eb1639d89a1b1ba487cda220748d
Anushree <anushreebhatt357@gmail.com>
2025-03-21 15:39:50 -0400

    logging - slf4j

--- src/main/java/com/practice/springboot/StudyGrind/login/LoginController.java
+++ src/main/java/com/practice/springboot/StudyGrind/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.practice.springboot.StudyGrind.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,10 @@ public class LoginController {
     //http://localhost:8080/login?name=Anushree Bhat
     @RequestMapping("/login")
     public String goToLogin(@RequestParam String name, ModelMap model){
-//        System.out.println("Request param name is:"+name);
-//        model.put("user_name", name);
-        model.put("user_name", name); //Not recommended. USe logging in next step.
+        System.out.println("Request param name is:"+name); //Not recommended. USe logging in next step.
+        Logger logger = LoggerFactory.getLogger(getClass());
+        logger.debug("Request param - name = {}",name);
+        model.put("user_name", name);
         return "login";
     }
 }
