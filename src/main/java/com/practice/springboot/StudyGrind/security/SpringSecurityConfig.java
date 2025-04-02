de83120802a2a68b9332e434391452fcc5fb5621
Anushree <anushreebhatt357@gmail.com>
2025-04-02 14:31:11 -0400

    Added Password Encoder

--- src/main/java/com/practice/springboot/StudyGrind/security/SpringSecurityConfig.java
+++ src/main/java/com/practice/springboot/StudyGrind/security/SpringSecurityConfig.java
@@ -3,35 +3,62 @@ package com.practice.springboot.StudyGrind.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.function.Function;
 
 @Component
 public class SpringSecurityConfig {
 
+
     @Bean
-    public InMemoryUserDetailsManager createUserDetailsManager(){
-        User.UserBuilder user=  User.withDefaultPasswordEncoder();
-        UserDetails userDetails = user
-                                .username("anushree")
-                                .password("anu@123")
+    protected InMemoryUserDetailsManager createUserDetailsManager(){
+        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+        UserDetails userDetails1 = User.builder().passwordEncoder(passwordEncoder)
+                                .username("user")
+                                .password("user@123")
+                                .roles("USER")
                                 .build();
 
-        return new InMemoryUserDetailsManager(userDetails);
+        UserDetails userDetails2 = User.builder()
+                                .passwordEncoder(passwordEncoder)
+                                .username("admin")
+                                .password("admin@123")
+                                .roles("ADMIN")
+                                .build();
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
     }
 
-//    // as password prefixAlternate-way - to use {noop}
+//    //Alternate way of doing
 //    @Bean
-//    public InMemoryUserDetailsManager createUserDetailsManager(){
-//        User.UserBuilder user=  User.builder();
-//        UserDetails userDetails = user
-//                .username("anushree")
-//                .password("{noop}anu@123")
+//    protected InMemoryUserDetailsManager createUserDetailsManager(){
+//        UserDetails userDetails1 = User.builder()
+//                .username("user")
+//                .password(passwordEncoder().encode("user@123"))
+//                .roles("USER")
 //                .build();
 //
-//        return new InMemoryUserDetailsManager(userDetails);
+//        UserDetails userDetails2 = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin@123"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
 //    }
+
+
+
+    @Bean
+    protected PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
 
 }
